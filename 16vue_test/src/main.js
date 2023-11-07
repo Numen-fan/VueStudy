76c699c7306612f153e5dcf4738bf402830c475b
Numen_fan <1414340590@qq.com>
2023-11-07 23:53:02 +0800

    element-ui

--- 16vue_test/src/main.js
+++ 16vue_test/src/main.js
@@ -12,6 +12,11 @@ import AppCategory from "@/AppCategory.vue";
 import AppCount from "@/AppCount.vue";
 import AppRouter from "@/AppRouter.vue";
 import Vuex from 'vuex'
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+
+// 按需引入，css按需自己导入
+import {Button, Row, DatePicker} from "element-ui";
 
 // 引入插件
 import plugins from "@/plugins";
@@ -28,17 +33,22 @@ Vue.use(VueRouter)
 // 关闭vue的生产提示
 Vue.config.productionTip = false
 Vue.use(plugins)
+// Vue.use(ElementUI);
+
+Vue.component(Button.name, Button)
+Vue.component(Row.name, Row)
+Vue.component(DatePicker.name, DatePicker)
 
 // 创建vue的实例对象 ----vm
 new Vue({
-  // el: '#app', // 和后面的mount('#app')效果一致
-  // 完成了将App组件放入容器中的功能
-  render: h => h(AppElementUI),
-  // template:'<h1>你好</h1>',
-  // components:{App}
-  store,
-  router,
-  beforeCreate() {
-    Vue.prototype.$bus = this; // 安装全局事件总线
-  }
+    // el: '#app', // 和后面的mount('#app')效果一致
+    // 完成了将App组件放入容器中的功能
+    render: h => h(AppElementUI),
+    // template:'<h1>你好</h1>',
+    // components:{App}
+    store,
+    router,
+    beforeCreate() {
+        Vue.prototype.$bus = this; // 安装全局事件总线
+    }
 }).$mount('#app')
